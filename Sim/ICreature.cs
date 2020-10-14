56ffc987be01ddbc180b850a365e749d39e52cab
Nac <teampavilion@gmail.com>
2020-10-14 17:32:45 +0200

    [^] Fixed the documentation.

--- Sim/ICreature.cs
+++ Sim/ICreature.cs
@@ -10,63 +10,63 @@ namespace Sim
   public interface IOrganism : IUnit
   {
     /// <summary>
-    /// Raised when the creature has been revived.
+    /// Raised when the organism has been revived.
     /// </summary>
     event EventHandler Revived;
 
     /// <summary>
-    /// Raised when the creature has been killed.
+    /// Raised when the organism has been killed.
     /// </summary>
     event EventHandler Killed;
 
     /// <summary>
-    /// Raised when the creature has been healed.
+    /// Raised when the organism has been healed.
     /// </summary>
     event EventHandler Healed;
 
 
     /// <summary>
-    /// Gets the last attacker of the creature.
+    /// Gets the last attacker of the organism.
     /// </summary>
     IOrganism LastAttacker { get; }
 
     /// <summary>
-    /// Gets the last killer of the creature.
+    /// Gets the last killer of the organism.
     /// </summary>
     IOrganism LastKiller { get; }
 
 
     /// <summary>
-    /// Gets a value indicating whether the creature is dead.
+    /// Gets a value indicating whether the organism is dead.
     /// </summary>
     bool IsDead { get; }
 
     /// <summary>
-    /// Gets a value indicating whether the creature is alive.
+    /// Gets a value indicating whether the organism is alive.
     /// </summary>
     bool IsAlive { get; }
 
 
     /// <summary>
-    /// Gets a value indicating whether the creature was killed.
+    /// Gets a value indicating whether the organism was killed.
     /// </summary>
     bool WasKilled { get; }
 
 
     /// <summary>
-    /// Kills the creature, specifying the killer.
+    /// Kills the organism, specifying the killer.
     /// </summary>
     /// <param name="killer">The killer.</param>
     void Kill([NotNull] IOrganism killer);
 
     /// <summary>
-    /// Revives the creature, specifying the reviver.
+    /// Revives the organism, specifying the reviver.
     /// </summary>
     /// <param name="reviver">The reviver.</param>
     void Revive([NotNull] IOrganism reviver);
 
     /// <summary>
-    /// Heals the creature, specifying the healer.
+    /// Heals the organism, specifying the healer.
     /// </summary>
     /// <param name="healer">The healer.</param>
     /// <param name="amount">The healing amount (hp).</param>
